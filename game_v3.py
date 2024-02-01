77b6cb981fae1caa24c4a39abf5912504748453c
otvorot <plikunoveu@gmail.com>
2024-02-01 21:13:46 +0400

    проект 0
    проект

---
+++ game_v3.py
@@ -0,0 +1,58 @@
+
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    
+    count = 0 # счётчик
+    number = np.random.randint(1, 101) # загаданное число
+    top = 101 # верхняя граница
+    bot = 1 # нижняя граница
+    
+    # цикл выполняет отсеивание половины диапазона
+    # загаданного числа
+    while True:
+        count += 1
+        mid = (top + bot) // 2
+        if number > mid: bot = mid
+        elif number < mid: top = mid
+        else: break
+        
+    return count
+        
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
+    return score
+
+
+score_game(random_predict)
--- ggg.py
+++
@@ -1,2 +0,0 @@
-print('lupa')
-a = 'lupa'
